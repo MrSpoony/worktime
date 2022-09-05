b6e666028a49c640fd53513c774721eb12ed4f66
Kimi Loeffel <kimi.loeffel@bit.admin.ch>
2022-09-05 16:17:22 +0200

    initial version

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/MrSpoony/worktime
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	start := flag.String("s", "08:00", "start time")
+	end := flag.String("e", "17:00", "end time")
+	pause := flag.String("p", "00:30", "pause")
+	flag.Parse()
+
+	starttime, isValid := validateHoursAndMinutes(*start, false)
+	if !isValid {
+		fmt.Println("Starting time is malformed")
+	}
+	endtime, isValid := validateHoursAndMinutes(*end, false)
+	if !isValid {
+		fmt.Println("Starting time is malformed")
+	}
+	pausetime, isValid := validateHoursAndMinutes(*pause, true)
+	if !isValid {
+		fmt.Println("Starting time is malformed")
+	}
+
+	timestart := createTimeDifference(starttime)
+	timeend := createTimeDifference(endtime)
+	timepause := createTimeDifference(pausetime)
+	fmt.Println(timestart, timeend, timepause)
+	now := time.Now()
+	futuretime := now.Add(timeend).Add(-timestart).Add(-timepause)
+	diff := futuretime.Sub(now)
+	hours := diff.Hours()
+	fmt.Println(diff)
+	fmt.Printf("%.2f\n", hours)
+}
+
+func validateHoursAndMinutes(input string, minutesAsStandard bool) ([2]int, bool) {
+	inputtime := strings.Split(input, ":")
+	if len(inputtime) > 2 {
+		return [2]int{0, 0}, false
+	}
+	if len(inputtime) == 1 {
+		if len(inputtime[0]) > 2 {
+			return validateHoursAndMinutes(input[:2]+":"+input[2:], minutesAsStandard)
+		}
+		stuff, err := strconv.Atoi(inputtime[0])
+		if err != nil {
+			return [2]int{0, 0}, false
+		}
+		if minutesAsStandard {
+			if stuff >= 60 {
+				return [2]int{0, 0}, false
+			}
+			return [2]int{0, stuff}, true
+		}
+		if stuff > 24 {
+			return [2]int{0, 0}, false
+		}
+		return [2]int{stuff, 0}, true
+	}
+	hours, err := strconv.Atoi(inputtime[0])
+	if err != nil {
+		return [2]int{0, 0}, false
+	}
+	minutes, err := strconv.Atoi(inputtime[1])
+	if err != nil {
+		return [2]int{0, 0}, false
+	}
+	if minutes >= 60 {
+		return [2]int{0, 0}, false
+	}
+	if hours > 24 {
+		return [2]int{0, 0}, false
+	}
+	return [2]int{hours, minutes}, true
+}
+
+func createTimeDifference(tm [2]int) time.Duration {
+	now := time.Now()
+	duration := now.Add(time.Hour*time.Duration(tm[0]) + time.Minute*time.Duration(tm[1])).Sub(now)
+	return duration
+}
